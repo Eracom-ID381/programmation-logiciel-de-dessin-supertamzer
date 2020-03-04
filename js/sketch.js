bfd825fc17a8167faf75eeffd268dc064a7c1a49
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-03-04 14:59:06 +0100

    logiciel batiment orange

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,18 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
-    noStroke();
+    stroke(2);
 }
 
 function draw() {
     fill(255);
-    rect(mouseX, mouseY, 50, 50);
+    noFill();
+    strokeWeight(1);
+    stroke(255, 102, 0);
+    curve(width / 2, height / 2, width / 2, mouseY, mouseX, mouseY, mouseY, mouseX);
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
 }
-
-if mouseease
