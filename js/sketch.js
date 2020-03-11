3a32ad5c0f1b623874e4c0cb48aea7fb736c7a78
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-03-11 15:53:21 +0100

    forme 1 + forme 2

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,5 @@
+let mode = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
@@ -5,13 +7,39 @@ function setup() {
 }
 
 function draw() {
-    fill(255);
     noFill();
     strokeWeight(1);
     stroke(255, 102, 0);
-    rectMode(CORNERS);
-    rect(mouseX, mouseY, mouseX + 100, mouseY + 200);
+    //curve(width / 2, height / 2, mouseX, height - 100, mouseX, mouseY, mouseY, mouseX);
+    //mouseY < height - 100
+    if (mouseIsPressed) {
+        if (mode == 0 && mouseY < 500) {
+
+            curve(width / 2, height / 2, mouseX, 500, mouseX, mouseY, mouseY, mouseX);
+
+        }
+        if (mode == 1) {
+            line(mouseX, mouseY, mouseX, mouseY + height)
+            line(mouseX, mouseY, mouseX + 250, mouseY);
+        }
+    }
+
+
+
 }
+
+function keyPressed() {
+    if (keyCode === BACKSPACE) {
+        background(0);
+    }
+    if (key == "1") {
+        mode = 0;
+    }
+    if (key == "2") {
+        mode = 1;
+    }
+}
+
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
