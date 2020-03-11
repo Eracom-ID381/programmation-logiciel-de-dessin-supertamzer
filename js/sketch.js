98f11ac14d1cda48bf88a2cb72535d4f2371a976
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-03-11 16:21:47 +0100

    forme 1 + nouvelle 2

--- js/sketch.js
+++ js/sketch.js
@@ -9,18 +9,20 @@ function setup() {
 function draw() {
     noFill();
     strokeWeight(1);
-    stroke(255, 102, 0);
     //curve(width / 2, height / 2, mouseX, height - 100, mouseX, mouseY, mouseY, mouseX);
     //mouseY < height - 100
     if (mouseIsPressed) {
         if (mode == 0 && mouseY < 500) {
-
+            stroke(255, 102, 0);
             curve(width / 2, height / 2, mouseX, 500, mouseX, mouseY, mouseY, mouseX);
 
         }
         if (mode == 1) {
-            line(mouseX, mouseY, mouseX, mouseY + height)
-            line(mouseX, mouseY, mouseX + 250, mouseY);
+            stroke(0, 255, 0);
+            //line(mouseX, mouseY, mouseX, mouseY + height)
+            //line(mouseX, mouseY, mouseX + 250, mouseY);
+            //line(mouseX + 250, mouseY, mouseX + 250, mouseY + height)
+            curve(width / 2, height / 2, width / 2, mouseY, mouseX, mouseY, mouseY, mouseX);
         }
     }
 
